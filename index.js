b1e3c209b3374f65afff4a04fee30ecdff71002b
albiummid <albi.ummid@gmail.com>
2021-03-31 19:27:27 +0600

    primary work done

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const port = 5000 || process.env.PORT;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config()
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.get('/', (req, res) => {
+  res.send('This is port 5000')
+})
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gpn2l.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const booksCollection = client.db("bongoLibrary").collection("books");
+  console.log("database connected , error:",err);
+  
+  app.post('/addProduct', (req, res) => {
+    const productInfo = req.body;
+    console.log("adding new product", productInfo);
+    booksCollection.insertOne(productInfo)
+      .then(result => {
+        console.log(result.insertedCount, 'insertedCount');
+        res.send(result.insertedCount > 0)
+    })
+  })
+  
+  app.get('/books',(req, res)=> {
+    booksCollection.find({})
+      .toArray((err, documents) => {
+      res.send(documents)
+    })
+  })
+
+});
+
+app.listen(port)
